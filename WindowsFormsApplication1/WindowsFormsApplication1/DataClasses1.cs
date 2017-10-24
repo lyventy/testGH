55a4d1d0fc638862d74adebe62a1b20b68b26c22
lyventy <lyventy@gmail.com>
2017-10-24 20:44:44 +0700

    lan 1

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/DataClasses1.cs
@@ -0,0 +1,6 @@
+namespace WindowsFormsApplication1
+{
+    partial class DataClasses1DataContext
+    {
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            loginuser lgs = new loginuser
+            {
+                user1 = textBox1.Text,
+                pass = textBox2.Text
+            };
+            DataClasses1DataContext da = new DataClasses1DataContext();
+            da.loginusers.InsertOnSubmit(lgs);
+            da.SubmitChanges();
+
+        }
+    }
+}
